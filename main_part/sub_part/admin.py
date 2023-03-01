f6b33b6c8021177731f30cc4a9c8eb065e0a4a7b
gokulkumar <gokulkumar4903@gmail.com>
2023-03-01 07:49:42 -0800

    django added

---
+++ main_part/sub_part/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(register_data)
+admin.site.register(login_data)
+admin.site.register(admin_data)
+admin.site.register(deleted_data)
